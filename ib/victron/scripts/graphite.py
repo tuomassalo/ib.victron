66a2e6744335894337811507c2d5dd5997d04099
Tuomas Salo <tuomas.salo@iki.fi>
2016-08-04 00:09:33 +0300

    fix: change graphite path to <hostname>.inverter.<key>

--- ib/victron/scripts/graphite.py
+++ ib/victron/scripts/graphite.py
@@ -12,7 +12,7 @@ def main():
     port = Serial(options.port, options.baudrate, timeout=options.timeout)
     mk2 = MK2(port).start()
 
-    g = graphitesend.init(graphite_server='127.0.0.1', group='mppt', prefix='')
+    g = graphitesend.init(graphite_server='127.0.0.1', group='inverter', prefix='')
 
     try:
         while True:
